0985883723539282b8c3ee916a21be94aca12ed1
amaciej <amaciejewski123@gmail.com>
2019-01-16 12:14:03 +0100

    Update zad2.py

--- Zadania1/zad2.py
+++ Zadania1/zad2.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 def function1():
     num = input("Podaj dowolna liczbe ")    # użytownik wprowadza liczbę
+    num2 = input("Podaj drugą liczbę ")
     num = float(num)
-    print("Liczba: {}".format(num))         # Wyswietlenie wprowadzonej liczby
+    print("Liczby: {}{}".format(num, num2))         # Wyswietlenie wprowadzonej liczby
 function1()
 
